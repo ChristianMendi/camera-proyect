64757a93e540e92484d54c0e9d890c38c4435233
christianmendieta82 <christianmendieta82@gmail.com>
2025-03-05 16:19:13 +0000

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cameraproyect.men',
+  appName: 'CameraApp',
+  webDir: 'dist'
+};
+
+export default config;
