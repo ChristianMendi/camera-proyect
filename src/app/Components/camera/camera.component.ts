8250d996e9d2b29c3d1f559769efc846b9088bf0
christianmendieta82 <christianmendieta82@gmail.com>
2025-03-05 17:13:02 +0000

    Galeria

--- src/app/Components/camera/camera.component.ts
+++ src/app/Components/camera/camera.component.ts
@@ -2,7 +2,6 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CameraService } from '../../Services/camera.service';
 
-
 @Component({
   selector: 'app-camera',
   standalone: true,
@@ -12,25 +11,32 @@ import { CameraService } from '../../Services/camera.service';
 })
 export class CameraComponent {
   cameraService: CameraService = inject(CameraService);
-  imgUrl: string = '';
+  imgUrls: string[] = []; // Arreglo para almacenar imágenes
   errorMessage: string = '';
   loading: boolean = false;
-  
+
   async takePicture() {
     this.errorMessage = ''; // Limpiar mensajes de error anteriores
-    
+
     try {
       this.loading = true;
-      this.imgUrl = await this.cameraService.takePicture();
-      if (!this.imgUrl) {
+      const newImgUrl = await this.cameraService.takePicture();
+      
+      if (!newImgUrl) {
         throw new Error('No se obtuvo una imagen válida');
       }
+
+      // Agregar la nueva imagen al arreglo, asegurando un máximo de 5 imágenes
+      this.imgUrls.unshift(newImgUrl);
+      if (this.imgUrls.length > 5) {
+        this.imgUrls.pop();
+      }
+
       await new Promise(resolve => setTimeout(resolve, 100));
       this.loading = false;
     } catch (error) {
       console.error('Error al capturar imagen:', error);
       this.errorMessage = String(error);
-      this.imgUrl = '';
       this.loading = false;
     }
   }
