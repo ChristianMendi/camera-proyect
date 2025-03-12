2ae3e25df57cfd5e352ab9aa766f7a7f08954cb9
christianmendieta82 <christianmendieta82@gmail.com>
2025-03-12 17:25:25 +0000

    Appppppppppp

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.cameraproyect.men',
   appName: 'CameraApp',
-  webDir: './dist/myapp/browser'
+  webDir: 'dist/myapp/browser'
 };
 
 export default config;
